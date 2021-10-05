17c48403fb24bbb0f532d38126d1f4c847d025f7
goransh02 <81700605+goransh02@users.noreply.github.com>
2021-10-05 16:43:55 +0530

    a

--- app.js
+++
@@ -1,8 +0,0 @@
-"http://192.168.0.108/get_items"
-function remove(el) {
-      var element = el;
-      element.remove();
-    }
-    $('.close-div').click(function(){
-      $(this).parent().parent().remove();
-   });
