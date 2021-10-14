6ff403078a6cd5d2186e14a66863a088172a9888
goransh02 <81700605+goransh02@users.noreply.github.com>
2021-10-14 21:59:13 +0530

    dd

---
+++ src/app/final/final.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import  { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-final',
+  templateUrl: './final.component.html',
+  styleUrls: ['./final.component.css']
+})
+
+export class FinalComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+    
+
+
+
+
+
+
+  
+
+}
+    
